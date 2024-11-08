90254c7b971196768f0767ff61049cb1a6631003
matos <matos.gustavo@outlook.com>
2024-11-07 23:10:47 -0300

    finalização da aula 117: seção 08, inserindo dados no banco mysql

---
+++ 08_integracao_com_mysql/aula_117_inserindo_dados_no_banco/server.js
@@ -0,0 +1,69 @@
+/**
+ * npm init -y
+ * npm install express express-handlebars mysql2
+ * npm install --save-dev nodemon
+ */
+
+import express from 'express';
+import { create } from 'express-handlebars';
+import mysql from 'mysql2';
+
+const port = 3008;
+
+// Configura o Handlebars
+const hbs = create({ 
+    partialsDir: ['views/partials']
+});
+
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json());
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
+
+    connection.query(sql, function(err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.redirect('/')
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+const connection = mysql.createConnection({
+    host: '192.168.100.215',
+    user: 'matos',
+    password: 'Matos@123',
+    database: 'nodemysql_aula_115'
+});
+  
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar:', err.stack);
+        return;
+    }
+    console.log('Conexão com MySQL realizada com sucesso!');
+
+    app.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`);
+    });
+});
