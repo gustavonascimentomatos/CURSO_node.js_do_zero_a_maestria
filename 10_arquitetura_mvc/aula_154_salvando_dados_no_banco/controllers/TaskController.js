a9ed26a38bdaf10ee1e08912f9e2c4d2c71394af
matos <matos.gustavo@outlook.com>
2024-11-22 23:59:19 -0300

    finalização da aula 155: seção 10, resgatando dados

---
+++ 10_arquitetura_mvc/aula_154_salvando_dados_no_banco/controllers/TaskController.js
@@ -0,0 +1,27 @@
+import Task from '../models/Task.js';
+
+class TaskController {
+    
+    static createTask(req, res) {
+        res.render('tasks/create');
+    }
+
+    static showTask(req, res) {
+        res.render('tasks/all');
+    }
+
+    static async createTaskSave(req, res) {
+        
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        await Task.create(task);
+        res.redirect('/tasks');
+    }
+
+}
+
+export default TaskController;
---
+++ 10_arquitetura_mvc/aula_154_salvando_dados_no_banco/routes/tasksRouters.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import TaskController from '../controllers/TaskController.js'
+
+const router = express.Router();
+
+router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave)
+router.get('/', TaskController.showTask);
+
+export default router;
